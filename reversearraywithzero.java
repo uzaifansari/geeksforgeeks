95cf0a77385022ca013ba16735dc73de68591e41
Uzaif Ansari <114265175+uzaifansari@users.noreply.github.com>
2022-10-22 18:04:13 +0530

    Create reversearraywithzero.java

---
+++ reversearraywithzero.java
@@ -0,0 +1,17 @@
+class Solution {
+    //Function to reverse every sub-array group of size k.
+    void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+     for(int i=0;i<n;i=i+k){
+          int left=i;
+          int right=Math.min(n-1,i+k-1);
+          int temp=0;
+          while(left<right){
+              temp=arr.get(left);
+              arr.set(left,arr.get(right));
+              arr.set(right,temp);
+              left++;
+              right--;
+          }
+        }
+    }
+}
